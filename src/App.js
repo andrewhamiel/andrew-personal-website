5a732a7b4575d04eb16ff86022f2d7c4215d16ac
andrewhamiel <andrew.hamiel@gmail.com>
2020-10-12 21:57:49 -0500

    registration page, routing back to home page, registration alerts

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 // App.js
-import React from 'react';
-import {BrowserRouter as Router, Link} from 'react-router-dom'
+import React, {useState} from 'react';
+import {BrowserRouter as Router, Switch} from 'react-router-dom'
 import { Route } from 'react-router';
 import HomePage from './components/HomePage';
 import RegistrationPage from './components/RegistrationPage';
-const App = () => {
-  return (
-    <section className="App">
-      <Router>
+import RegistrationAlerts from './components/Alerts/RegistrationAlerts';
+function App() {
+  const [title, updateTitle] = useState(null);
 
-        <Route exact path="/registration" component={RegistrationPage} />
-        <Route exact path="/" component={HomePage} />
+  const [errorMessage, updateErrorMessage] = useState(null);
+
+  return (
+      <Router>
+        <Switch>
+          <Route exact path="/registration">
+            <RegistrationPage showError={updateErrorMessage} updateTitle={updateTitle}/>
+          </Route>
+          <Route exact path="/">
+            <HomePage showError={updateErrorMessage} updateTitle={updateTitle}/>
+          </Route>
+        </Switch>
+        <RegistrationAlerts errorMessage={errorMessage} hideError={updateErrorMessage} />
       </Router>
-    </section>
   );
-};
+}
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import '../index.css';
+
 function Header() {
     return(
         <nav class="navbar navbar-dark bg-primary">
             <div className="row col-12 d-flex justify-content-center text-white">
-            <span className="h3">Register</span>
+            <span className="h3" href="/register"><a href="/registration" id="header">Register</a></span>
             </div>
         </nav>
     )
